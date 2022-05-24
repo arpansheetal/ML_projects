58e3fc1ce10d52fa58e4c4ae79997e4c23a62aea
arpansheetal <arpansheetal@gmail.com>
2022-05-25 01:42:28 +0530

    Created function to check none

--- test.py
+++ test.py
@@ -2,6 +2,10 @@ class ABC:
     def __init__(self) -> None:
         self.test_var = None
 
+    def check_None(self):
+        # Check test var for None
+        return self.test_var == None
+
 if __name__ == '__main__':
     v = ABC()
-    print(v.test_var == None)
+    print(v.check_None())
